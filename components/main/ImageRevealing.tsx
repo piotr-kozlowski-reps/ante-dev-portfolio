ba820d372e57d098e0c0ec970604de808744fc70
piotr <pegaz171@o2.pl>
2022-10-02 21:34:12 +0200

    update ...

--- components/main/ImageRevealing.tsx
+++ components/main/ImageRevealing.tsx
@@ -1,79 +1,90 @@
 import * as React from "react";
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 interface Props {
   // children: React.ReactNode;
 }
 const ImageRevealing: React.FunctionComponent<Props> = (props: Props) => {
+  const imageOff = React.useRef(null);
+  const imageOn = React.useRef(null);
+
+  const imagesArray = [imageOff, imageOn];
+
+  React.useEffect(() => {
+    gsap.fromTo(
+      imageOn.current,
+      { autoAlpha: 0 },
+      { autoAlpha: 1, duration: 3 }
+    );
+
+    imagesArray.forEach((image) => {
+      console.log(image);
+
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: document.querySelector("body"),
+          start: "center center",
+          // end: () => "+=100",
+          pin: true,
+          anticipatePin: 1,
+          scrub: true,
+        },
+      });
+
+      tl.fromTo(imageOff, { xPercent: 100, x: 0 }, { xPercent: 0 }).fromTo(
+        imageOn,
+        { xPercent: -100, x: 0 },
+        { xPercent: 0 },
+        0
+      );
+    });
+
+    // gsap.utils.toArray(".comparisonSection").forEach((section) => {
+    //   let tl = gsap.timeline({
+    //     scrollTrigger: {
+    //       trigger: section,
+    //       start: "center center",
+    //       // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+    //       end: () => "+=" + section.offsetWidth,
+    //       scrub: true,
+    //       pin: true,
+    //       anticipatePin: 1,
+    //     },
+    //     defaults: { ease: "none" },
+    //   });
+    //   // animate the container one way...
+    //   tl.fromTo(
+    //     section.querySelector(".afterImage"),
+    //     { xPercent: 100, x: 0 },
+    //     { xPercent: 0 }
+    //   )
+    //     // ...and the image the opposite way (at the same time)
+    //     .fromTo(
+    //       section.querySelector(".afterImage img"),
+    //       { xPercent: -100, x: 0 },
+    //       { xPercent: 0 },
+    //       0
+    //     );
+    // });
+  }, []);
+
   return (
     <React.Fragment>
-      <div className="fixed -left-1/4">
-        <Image src={"/images/face__off.jpg"} alt="" width={800} height={800} />
-      </div>
-      <div className="fixed -left-1/4">
-        <Image src={"/images/face__on.jpg"} alt="" width={800} height={800} />
-      </div>
-      <div className="text-5xl">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          at natus consectetur facere cupiditate commodi dolorem voluptatum
-          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
-          voluptatibus nostrum.
-        </p>
+      <div className="">
+        <div ref={imageOff} className="fixed ">
+          <Image
+            src={"/images/face__off.jpg"}
+            alt=""
+            width={800}
+            height={800}
+          />
+        </div>
+        <div ref={imageOn} className="fixed ">
+          <Image src={"/images/face__on.jpg"} alt="" width={800} height={800} />
+        </div>
       </div>
     </React.Fragment>
   );
--- components/main/MainLayout.tsx
+++ components/main/MainLayout.tsx
@@ -5,7 +5,88 @@ interface Props {
   // children: React.ReactNode;
 }
 const MainLayout: React.FunctionComponent<Props> = (props: Props) => {
-  return <ImageRevealing />;
+  return (
+    <React.Fragment>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <div>
+          <h1 className="text-5xl uppercase">asdcadfs</h1>
+        </div>
+        <div>
+          <h3 className="text-2xl">asdcadfs</h3>
+        </div>
+        <div>
+          <p className="text-sm px-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            architecto provident excepturi et iure aliquam consequuntur soluta
+            molestias cupiditate labore.
+          </p>
+        </div>
+      </div>
+      {/* <ImageRevealing /> */}
+      <div className="text-5xl">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+          at natus consectetur facere cupiditate commodi dolorem voluptatum
+          totam impedit iste ab a qui eaque fugit, nam aspernatur dolor
+          voluptatibus nostrum.
+        </p>
+      </div>
+    </React.Fragment>
+  );
 };
 export default MainLayout;
 
